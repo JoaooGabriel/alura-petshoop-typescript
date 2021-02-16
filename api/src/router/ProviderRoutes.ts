abab13c6d7e929862d613f360dcf83b9773cbed5
João Gabriel <caetanojoaogabriel@gmail.com>
2021-02-16 11:05:57 -0300

    feat: add method put provider

--- api/src/router/ProviderRoutes.ts
+++ api/src/router/ProviderRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.post('/providers', ProviderController.store);
 router.get('/providers', ProviderController.index);
 router.get('/providers/:id', ProviderController.show);
+router.put('/providers/:id', ProviderController.update);
 router.delete('/providers/:id', ProviderController.delete);
 
 export default router;

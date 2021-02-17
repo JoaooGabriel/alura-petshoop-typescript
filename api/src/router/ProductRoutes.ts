d2c745a4fcae9dadbd78316725d2e4c67686e818
João Gabriel <caetanojoaogabriel@gmail.com>
2021-02-17 17:10:13 -0300

    feat: create method routes

--- api/src/router/ProductRoutes.ts
+++ api/src/router/ProductRoutes.ts
@@ -4,11 +4,13 @@ import ProductController from '@controllers/ProductController';
 
 const router = Router();
 
+router.post('/products/sell/:id', ProductController.sell);
 router.post('/products', ProductController.store);
 router.get('/products', ProductController.index);
 router.get('/products/:id', ProductController.show);
 router.get('/products/providers/:id', ProductController.showByProvidersId);
-router.put('/products', ProductController.update);
-router.delete('/products/:id/providers/:id', ProductController.delete);
+router.get('/products/:idProc/providers/:idProv', ProductController.showProductByProvidersId);
+router.put('/products/:id', ProductController.update);
+router.delete('/products/:id', ProductController.delete);
 
 export default router;
